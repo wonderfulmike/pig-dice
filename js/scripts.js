7fcd053078b640a19808ceef651a7b2226311f67
wonderfulmike <mike76231@gmail.com>
2018-07-09 10:57:53 +0300

    Added linear gradient to container and more styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activePlayer, dice;
+/*var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
@@ -6,7 +6,7 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-$(document).ready(function(){
-  $(".current-score#current-0").innerHTML = dice;
+$(document).ready(function(event){
+  $(".current-score#current-0").textContent = dice;
 
-});
+});*/
